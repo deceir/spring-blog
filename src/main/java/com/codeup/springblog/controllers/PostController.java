5a63c3fda6702a4a16345e5b66caf3687857e747
Aarion Ojeda <aarion.ojeda01@gmail.com>
2021-09-21 15:33:16 -0500

    completed view exerise

--- src/main/java/com/codeup/springblog/controllers/PostController.java
+++ src/main/java/com/codeup/springblog/controllers/PostController.java
@@ -1,24 +1,42 @@
 package com.codeup.springblog.controllers;
 
+import com.codeup.springblog.models.Post;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 public class PostController {
 
     @GetMapping("/posts")
-    @ResponseBody
-    public String showPosts() {
-        return "Viewing all posts.";
+
+    public String showPosts(Model model) {
+        List<Post> listOfPosts = new ArrayList<>();
+        Post p1 = new Post("Feeling funny", "today kinda sucks.");
+        Post p2 = new Post("Feeling funny", "today kinda sucks. x2");
+
+        listOfPosts.add(p1);
+        listOfPosts.add(p2);
+        model.addAttribute("listOfPosts", listOfPosts);
+        return "/posts/post_index";
     }
 
     @GetMapping("/posts/{id}")
-    @ResponseBody
-    public String showPostById(@PathVariable int id) {
-        return "Viewing post with id of: " + id;
+
+    public String showPostById(@PathVariable int id, Model model) {
+
+        Post post = new Post("i find it kinda funny", "i find it kinda sad.");
+
+        model.addAttribute("title", post.getTitle());
+        model.addAttribute("content", post.getContent());
+
+        return "/posts/view_post";
     }
 
     @GetMapping("/posts/create")
